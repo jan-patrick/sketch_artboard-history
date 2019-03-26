13424e93a4282a4e9002732d012dc74a4c277e72
jan-patrick <jan.schneider@hfg-gmuend.de>
2019-03-26 11:33:32 +0100

    read out new + old artboard one artboard change (on event begin + finish) + alert message context

--- src/my-action-listener.js
+++ src/my-action-listener.js
@@ -1,3 +1,15 @@
+var UI = require('sketch/ui')
+
 export function updateLastArtboard(context) {
-  context.actionContext.document.showMessage('artboard changed')
+  //context.actionContext.document.showMessage('artboard changed')
+  
+  //var document = require('sketch/dom').getSelectedDocument()
+  //context.actionContext.document.showMessage(document)
+
+  //var ter = String(context.oldArtboard)
+  //context.actionContext.document.showMessage(ter)
+
+  var t = String("old: " + context.actionContext.oldArtboard + "\n new: "+ context.actionContext.newArtboard)
+  context.actionContext.document.showMessage(t)
+  UI.alert('my title', String(context))
 }
