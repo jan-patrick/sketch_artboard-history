e2a807572cb5ec66006a7e7872197442d225df76
jan-patrick <jan.schneider@hfg-gmuend.de>
2019-03-29 16:03:45 +0100

    saving complete History + ability to jump backwards

--- src/commands.js
+++ src/commands.js
@@ -14,70 +14,71 @@ function sendMessageToBottom(dataBottom) {
 }
 
 export function goToLastArtboard(context) {
-    //sketch.UI.message('Hi.')
-    
-    //var lastArtboardSaved = Settings.settingForKey('lastArtboard')
-    //sendErrorMessage(lastArtboardSaved)
-    
-    //<MSArtboardGroup: 0x7fa9dd758120> goBackinArtboardHistory (4DC34C6A-77BD-4123-9EBD-543025331025)
-    //var layer = document.getLayerWithID("4DC34C6A-77BD-4123-9EBD-543025331025")
-    //context.actionContext.document.showMessage(String(layer))
-    //sendErrorMessage(layer)
-    //document.centerOnLayer(layer)
+  var lastSaved = Settings.settingForKey("lastArtboard")
+  Settings.setSettingForKey("lastArtboard", lastSaved.substring(lastSaved.indexOf(".") + 1))
+  var lastArtboardSaved = lastSaved.substring(0, lastSaved.indexOf("."))
 
-    //////
-    var lastArtboardSaved = Settings.settingForKey("lastArtboard")
-    lastArtboardSaved = lastArtboardSaved.substring(lastArtboardSaved.indexOf(".") + 1)
-    var actualArtboardSaved = Settings.settingForKey("actualArtboard")
-    actualArtboardSaved = actualArtboardSaved.substring(actualArtboardSaved.indexOf(".") + 1)
-    var document = require('sketch/dom').getSelectedDocument()
-    var layer = document.getLayerWithID(lastArtboardSaved)
-    document.centerOnLayer(layer)
-    Settings.setSettingForKey("lastArtboard", actualArtboardSaved)
-    Settings.setSettingForKey("actualArtboard", lastArtboardSaved)
+  var strNew = Settings.settingForKey("actualArtboard")
+  strNew = lastArtboardSaved + "." + strNew
+  Settings.setSettingForKey("actualArtboard", strNew)
+  var document = require('sketch/dom').getSelectedDocument()
+  var layer = document.getLayerWithID(lastArtboardSaved)
+  document.centerOnLayer(layer)
+  //sendErrorMessage(lastArtboardSaved)
+}
+
+export function showSavedArtboardHistory(context) {
+  sendErrorMessage(Settings.settingForKey("lastArtboard") + "\n\n" + Settings.settingForKey("actualArtboard"))
 }
 
 export function showSelectedLayerInfo(context) {
   var selection = context.selection;
   var layer = selection.firstObject();
-  sendErrorMessage(layer.objectID() +" class: "+layer.class() +" name: "+layer.name())
+  sendErrorMessage(layer.objectID() + " class: " + layer.class() + " name: " + layer.name())
 
   /////////
   var document = require('sketch/dom').getSelectedDocument()
   var layer = document.getLayerWithID(layer.objectID())
   //sendErrorMessage(JSON.stringify(layer))
   document.centerOnLayer(layer)
+}
 
+function doesStringIncludeThat(stringToCheck, stringCheckingWith) {
+  while (stringCheckingWith.length>2) {
+    var stringCheckingWithCutOut = stringCheckingWith.substring(0, stringCheckingWith.indexOf("."))
+    stringCheckingWith = stringCheckingWith.substring(stringCheckingWith.indexOf(".") + 1)
+    if (stringToCheck === stringCheckingWithCutOut) {
+      return true
+    }
+  }
+  return false
+}
+
+export function cleanupArtboardHistory(context) {
+  Settings.setSettingForKey("lastArtboard", "")
+  Settings.setSettingForKey("actualArtboard", "")
 }
 
 export function updateArtboardHistory(context) {
   // get last Artboard
-  var strOld = String(context.actionContext.oldArtboard)
-  var strOldArtboard = strOld.substring(strOld.indexOf("(") + 1)
+  var strOldArtboard = String(context.actionContext.oldArtboard)
+  strOldArtboard = strOldArtboard.substring(strOldArtboard.indexOf("(") + 1)
   strOldArtboard = strOldArtboard.substring(0, strOldArtboard.indexOf(")"))
-  
-  // get last document
-  var strOldDocument = strOld.substring(strOld.indexOf(" ") + 1)
-  strOldDocument = strOldDocument.substring(0, strOldDocument.indexOf(">"))
 
-  // save document + Artboard
-  strOld = strOldDocument + "." + strOldArtboard
+  // save last
+  var strOld = Settings.settingForKey("lastArtboard")
+  strOld = strOldArtboard + "." + strOld
   Settings.setSettingForKey("lastArtboard", strOld)
 
+  sendMessageToBottom(String(doesStringIncludeThat(String(strOldArtboard), String(strOld))))
 
-  // get new Artboard
-  var strNew = String(context.actionContext.newArtboard)
-  var strNewArtboard = strNew.substring(strNew.indexOf("(") + 1)
+  // get actual Artboard
+  var strNewArtboard = String(context.actionContext.newArtboard)
+  strNewArtboard = strNewArtboard.substring(strNewArtboard.indexOf("(") + 1)
   strNewArtboard = strNewArtboard.substring(0, strNewArtboard.indexOf(")"))
 
-  // get last document
-  var strNewDocument = strNew.substring(strNew.indexOf(" ") + 1)
-  strNewDocument = strNewDocument.substring(0, strNewDocument.indexOf(">"))
+  Settings.setSettingForKey("actualArtboard", strNewArtboard)
 
-  // save document + Artboard
-  strNew = strNewDocument + "." + strNewArtboard
-  Settings.setSettingForKey("actualArtboard", strNew)
-
-  //sendMessageToBottom(strNew)
+  //sendErrorMessage(strOld)
   //sendErrorMessage(context)
 }
