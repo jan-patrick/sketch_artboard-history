8b5e84151420211488201463c73572bb941bfe08
jan-patrick <jan.schneider@hfg-gmuend.de>
2019-04-04 14:35:36 +0200

    dev functions + working function in one release

--- src/commands.js
+++ src/commands.js
@@ -14,6 +14,34 @@ function sendMessageToBottom(dataBottom) {
 }
 
 export function goToLastArtboard(context) {
+  var lastArtboardSavedA = Settings.settingForKey("lastArtboardA")
+  //lastArtboardSavedA = lastArtboardSavedA.substring(lastArtboardSavedA.indexOf(".") + 1)
+  var actualArtboardSavedA = Settings.settingForKey("actualArtboardA")
+  //actualArtboardSavedA = actualArtboardSavedA.substring(actualArtboardSavedA.indexOf(".") + 1)
+  var documentA = require('sketch/dom').getSelectedDocument()
+  var layerA = documentA.getLayerWithID(lastArtboardSavedA)
+  documentA.centerOnLayer(layerA)
+  Settings.setSettingForKey("lastArtboardA", actualArtboardSavedA)
+  Settings.setSettingForKey("actualArtboardA", lastArtboardSavedA)
+}
+
+// DEV
+export function goToLastArtboardDEV(context) {
+  var lastSaved = Settings.settingForKey("lastArtboard")
+  Settings.setSettingForKey("lastArtboard", lastSaved.substring(lastSaved.indexOf(".") + 1))
+  var lastArtboardSaved = lastSaved.substring(0, lastSaved.indexOf("."))
+
+  var strNew = Settings.settingForKey("actualArtboard")
+  strNew = lastArtboardSaved + "." + strNew
+  Settings.setSettingForKey("actualArtboard", strNew)
+  var document = require('sketch/dom').getSelectedDocument()
+  var layer = document.getLayerWithID(lastArtboardSaved)
+  document.centerOnLayer(layer)
+  //sendErrorMessage(lastArtboardSaved)
+}
+
+// DEV
+export function goToNextArtboardDEV(context) {
   var lastSaved = Settings.settingForKey("lastArtboard")
   Settings.setSettingForKey("lastArtboard", lastSaved.substring(lastSaved.indexOf(".") + 1))
   var lastArtboardSaved = lastSaved.substring(0, lastSaved.indexOf("."))
@@ -60,6 +88,43 @@ export function cleanupArtboardHistory(context) {
 }
 
 export function updateArtboardHistory(context) {
+// get last Artboard
+var strOldA = String(context.actionContext.oldArtboard)
+var strOldArtboardA = strOldA.substring(strOldA.indexOf("(") + 1)
+strOldArtboardA = strOldArtboardA.substring(0, strOldArtboardA.indexOf(")"))
+
+// get last document
+var strOldDocumentA = strOldA.substring(strOldA.indexOf(" ") + 1)
+strOldDocumentA = strOldDocumentA.substring(0, strOldDocumentA.indexOf(">"))
+
+// save document + Artboard
+strOldA = strOldDocumentA + "." + strOldArtboardA
+Settings.setSettingForKey("lastArtboardA", strOldArtboardA)
+
+
+// get new Artboard
+var strNewA = String(context.actionContext.newArtboard)
+var strNewArtboardA = strNewA.substring(strNewA.indexOf("(") + 1)
+strNewArtboardA = strNewArtboardA.substring(0, strNewArtboardA.indexOf(")"))
+
+// get last document
+var strNewDocumentA = strNewA.substring(strNewA.indexOf(" ") + 1)
+strNewDocument = strNewDocument.substring(0, strNewDocumentA.indexOf(">"))
+
+// save document + Artboard
+strNewA = strNewDocumentA + "." + strNewArtboardA
+Settings.setSettingForKey("actualArtboardA", strOldArtboardA)
+
+
+
+
+
+
+
+  /////////////////////////
+  // DEVELOPMENT SECTION //
+  /////////////////////////
+
   // get last Artboard
   var strOldArtboard = String(context.actionContext.oldArtboard)
   strOldArtboard = strOldArtboard.substring(strOldArtboard.indexOf("(") + 1)
@@ -80,5 +145,5 @@ export function updateArtboardHistory(context) {
   Settings.setSettingForKey("actualArtboard", strNewArtboard)
 
   //sendErrorMessage(strOld)
-  sendErrorMessage(context)
+  //sendErrorMessage(context)
 }
