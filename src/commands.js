36ea14097fc18174b01ac96323c381840f9f36c7
jan-patrick <jan.schneider@hfg-gmuend.de>
2019-07-05 14:36:53 +0200

    forward update with back function

--- src/commands.js
+++ src/commands.js
@@ -305,41 +305,63 @@ export function goToLastArtboard() {
 
 export function goToNextArtboard() {
   var artboardHistory = getSavedSetting("ArtboardHistory")
-  var documentId = getDocumentId()
   var lastArtboardSavedP = ""
   var lastArtboardSavedA = ""
   var j = 0
   var b = 0
   var done = false
+  var artboardFound = false
+  var document = require('sketch/dom').getSelectedDocument()
   while (!done) {
     if (0 < artboardHistory.documents.length) {
       var documentFound = false
       for (var l = 0; l < artboardHistory.documents.length; l++) {
-        if (documentId === artboardHistory.documents[l].id) {
+        var selectedLayerTry = false
+        var countRuntimeO = 0
+        if (document.id === artboardHistory.documents[l].id) {
           documentFound = true
-          if (-1 === artboardHistory.documents[l].lastHistoryIndex) {
-            sendMessageToBottom("You have to go back first.")
-          }
-          var previousArtboardTime = 0
-
-          sendMessageToBottom("db")
-          for (var o = 0; o < artboardHistory.documents[l].storedHistory.length; o++) {
-            var countRuntimeO = 0
-            if (previousArtboardTime < artboardHistory.documents[l].storedHistory[o].id &&
-              artboardHistory.documents[l].lastHistoryIndex > artboardHistory.documents[l].storedHistory[o].id) {
-              previousArtboardTime = artboardHistory.documents[l].storedHistory[o].id
-              lastArtboardSavedP = artboardHistory.documents[l].storedHistory[o].page
-              lastArtboardSavedA = artboardHistory.documents[l].storedHistory[o].artboard
-              j = o
-              b = l
-              countRuntimeO++
+          if (isLayerSelected() && !selectedLayerTry) {
+            if (-1 === artboardHistory.documents[l].lastHistoryIndex) {
+              sendMessageToBottom("You have to go back first.")
+              return
             }
-          }
-          artboardHistory.documents[l].lastHistoryIndex = previousArtboardTime
-          if (0 <= countRuntimeO) {
-            artboardHistory.documents[l].lastMoveByUser = false
+            var previousArtboardTime = 0
+            //sendErrorMessage(artboardHistory.documents[l].lastMoveByUser)
+            for (var o = 0; o < artboardHistory.documents[l].storedHistory.length; o++) {
+              //sendErrorMessage("checks",previousArtboardTime +" < " + artboardHistory.documents[l].storedHistory[o].id + "\n\n" + artboardHistory.documents[l].lastHistoryIndex +" > "+ artboardHistory.documents[l].storedHistory[o].id)
+              if (previousArtboardTime < artboardHistory.documents[l].storedHistory[o].id &&
+                artboardHistory.documents[l].lastHistoryIndex > artboardHistory.documents[l].storedHistory[o].id) {
+                previousArtboardTime = artboardHistory.documents[l].storedHistory[o].id
+                lastArtboardSavedA = artboardHistory.documents[l].storedHistory[o].artboard
+                artboardFound = true
+                j = o
+                b = l
+                countRuntimeO++
+                //sendMessageToBottom(getCurrentTime()) // @jan 2
+              }
+              if (0 <= countRuntimeO) {
+                artboardHistory.documents[l].lastMoveByUser = false
+              } else {
+                artboardHistory.documents[l].lastMoveByUser = true
+              }
+            }
+            //sendErrorMessage("E lastArtboardSavedP",lastArtboardSavedP)
+            artboardHistory.documents[l].lastHistoryIndex = previousArtboardTime
           } else {
-            artboardHistory.documents[l].lastMoveByUser = true
+            selectedLayerTry = true
+            for (var k = 0; k < artboardHistory.documents[l].storedHistory.length; k++) {
+              //sendErrorMessage("checks",previousArtboardTime +" < " + artboardHistory.documents[l].storedHistory[o].id + "\n\n" + artboardHistory.documents[l].lastHistoryIndex +" > "+ artboardHistory.documents[l].storedHistory[o].id)
+              if (artboardHistory.documents[l].timestamp === artboardHistory.documents[l].storedHistory[k].id) {
+                lastArtboardSavedA = artboardHistory.documents[l].storedHistory[k].artboard
+                artboardHistory.documents[l].lastMoveByUser = false
+                artboardFound = true
+                j = k
+                b = l
+                countRuntimeO++
+                //sendMessageToBottom("found")
+                //sendErrorMessage("E countRuntimeO",countRuntimeO)
+              }
+            }
           }
         }
       }
@@ -350,6 +372,18 @@ export function goToNextArtboard() {
     } else {
       sendMessageToBottom("No History available")
       return
+    }
+
+    // finding page of Artboard
+    if (artboardFound) {
+      for (var a = 0; a < document.pages.length; a++) {
+        for (var c = 0; c < document.pages[a].layers.length; c++) {
+          if ("Artboard" === document.pages[a].layers[c].type && lastArtboardSavedA === document.pages[a].layers[c].id) {
+            lastArtboardSavedP = document.pages[a].id
+            //sendErrorMessage(document.pages[a].name, document.pages[a].layers[c].name)
+          }
+        }
+      }
     }
     var layerP = document.getLayerWithID(lastArtboardSavedP)
     var layerA = document.getLayerWithID(lastArtboardSavedA)
@@ -364,7 +398,6 @@ export function goToNextArtboard() {
       }
       done = true
     } else {
-      sendMessageToBottom("HERE")
       if (j >= 1) {
         artboardHistory.documents[b].storedHistory.splice(j, 1)
       } else {
